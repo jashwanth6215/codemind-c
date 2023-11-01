abf3fe6763254236ac98e670d0bc69fdfe62f171
jashwanth6215 <147476570+jashwanth6215@users.noreply.github.com>
2023-11-01 11:00:14 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%i%i",&a,&b);     for(int i=1;i<=b;i++)     {         if(a*i%b==0)         {             printf("%i",(a*b)/(a*i));             break;         }     } }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%i%i",&a,&b);
+    for(int i=1;i<=b;i++)
+    {
+        if(a*i%b==0)
+        {
+            printf("%i",(a*b)/(a*i));
+            break;
+        }
+    }
+}

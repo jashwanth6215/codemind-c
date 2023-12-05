a0e855e017e8b9a76e15f2d530debc4fcf7b4518
jashwanth6215 <147476570+jashwanth6215@users.noreply.github.com>
2023-12-05 18:51:44 +0530

    #include<stdio.h> int main() {     int n,j,i;     scanf("%i",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%i ",i);         }         printf(" ");     } } #include<stdio.h> int main() {     int n,j,i;     scanf("%i",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             printf("%i ",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_1.3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,j,i;
+    scanf("%i",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%i ",i);
+        }
+        printf("
+");
+    }
+}
+#include<stdio.h>
+int main()
+{
+    int n,j,i;
+    scanf("%i",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%i ",i);
+        }
+        printf("
+");
+    }
+}

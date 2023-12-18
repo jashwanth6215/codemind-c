8f843b70e93fa3d3ac45053d271a230703809f86
jashwanth6215 <147476570+jashwanth6215@users.noreply.github.com>
2023-12-18 10:40:47 +0530

    #include<stdio.h> int main() {     int a,sum=0;     scanf("%i",&a);     int arr[a];     for(int i=0;i<a;i++)     {         scanf("%i",&arr[a]);         if((arr[a])%2!=0)         {             sum+=arr[a];         }     }     printf("%i",sum); }

---
+++ Odd_Sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,sum=0;
+    scanf("%i",&a);
+    int arr[a];
+    for(int i=0;i<a;i++)
+    {
+        scanf("%i",&arr[a]);
+        if((arr[a])%2!=0)
+        {
+            sum+=arr[a];
+        }
+    }
+    printf("%i",sum);
+}

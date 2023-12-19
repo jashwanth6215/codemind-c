b227886cdd7fa966e926dc965402ce015f4a236b
jashwanth6215 <147476570+jashwanth6215@users.noreply.github.com>
2023-12-19 11:15:21 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,sum=0,c=0;     scanf("%i",&a);     int arr[a];     for(int i=0;i<a;i++)     {         scanf("%i",&arr[i]);         sum+=arr[i];     }     for(int i=0;i<a;i++)     {         if(arr[i]>=floor(sum/a))         {             c++;         }     }     printf("%i",c); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,sum=0,c=0;
+    scanf("%i",&a);
+    int arr[a];
+    for(int i=0;i<a;i++)
+    {
+        scanf("%i",&arr[i]);
+        sum+=arr[i];
+    }
+    for(int i=0;i<a;i++)
+    {
+        if(arr[i]>=floor(sum/a))
+        {
+            c++;
+        }
+    }
+    printf("%i",c);
+}

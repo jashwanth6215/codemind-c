48970ff749c31e82a66b0d6f0567647883197149
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-03-18 19:54:16 +0530

    #include<iostream> #include<iomanip> using namespace std; int main() {     int a;     cin >>a;     cout <<fixed<<setprecision(2);     cout <<a*(0.2777777778);//(1000/3600) }

---
+++ kmph_to_mps.c
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int a;
+    cin >>a;
+    cout <<fixed<<setprecision(2);
+    cout <<a*(0.2777777778);//(1000/3600)
+}

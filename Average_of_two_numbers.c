c5c6ef851e6ca625d35b9715e5d3298b5603458d
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-03-20 21:12:25 +0530

    #include<iostream> #include<iomanip> using namespace std; int main() {     float a,b;     cin >>a>>b;     cout <<fixed<<setprecision(2);     cout <<"Average of "<<int (a)<<" and "<<int (b)<<" is: "<<(a+b)/2; }

--- Average_of_two_numbers.c
+++ Average_of_two_numbers.c
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-    float  a,b;
+    float a,b;
     cin >>a>>b;
     cout <<fixed<<setprecision(2);
     cout <<"Average of "<<int (a)<<" and "<<int (b)<<" is: "<<(a+b)/2;

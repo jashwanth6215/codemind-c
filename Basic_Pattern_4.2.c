51ce4bcd3609ee126c3720c1ad0ed0b0073b7ccd
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-05-10 23:54:34 +0530

    #include<stdio.h>

---
+++ Basic_Pattern_4.2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%i",&a);
+    for(int i=1;i<=a;i++)
+    {
+        for(int j=1;j<=a;j++)
+        {
+            if(i%2!=0)
+            {
+                printf("1 ");
+            }
+            else
+            {
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+}
